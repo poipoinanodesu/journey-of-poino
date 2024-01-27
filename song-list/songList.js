7de70edbcf65af7859d4a6260bc464c94c147425
poipoinanodesu <154312691+poipoinanodesu@users.noreply.github.com>
2024-01-27 12:00:21 +0700

    Design draft, renew song generator (#1)
    * renew song generator
    
    * renew song generator
    
    * Delete assets/img/clients directory
    
    * Delete assets/img/portfolio directory
    
    * Delete assets/img/team directory
    
    * Delete assets/img/testimonials directory
    
    * Delete assets/img/about.jpg
    
    * Delete assets/img/hero-bg.jpg

---
+++ song-list/songList.js
@@ -0,0 +1,265 @@
+/*
+Song Name
+category: song category
+linkMusescore: Musescore link
+linkMega: raw link
+linkMegaComment: comment link
+*/
+const songCollection = {
+"Secrets of Wysteria - Steampianist ft.Oliver": {
+"category": "vocaloid",
+"linkMusescore": "https://musescore.com/user/18011801/scores/3619396",
+"linkMega": "https://mega.nz/file/SCgxiQCK#IjiAiFOmq7ftcmQJTKFK1TP7BEDyX-8Q-pl1gjoelfg",
+}, 
+
+"Ai Kotoba - Deco*27": {
+"category": "vocaloid",
+"linkMusescore": "https://musescore.com/user/18011801/scores/5271133",
+"linkMega": "https://mega.nz/file/aTYXiI7J#2mJ9JDh_tg6wZI1Ilgk0xqGIuKzZbUYqoBgtpTSDxRw",
+},
+
+"LiLium - Elfen Lied": {
+"category": "anime",
+"linkMusescore": "https://musescore.com/user/18011801/scores/5531763",
+"linkMega": "https://mega.nz/file/3OIUxYjD#o9r8pQPslNfKtvewNg7HZX1Ff2nd2uZOjdzefs4szt8",
+},
+
+"Ametsuyu - Elfen Lied": {
+"category": "anime",
+"linkMega": "https://mega.nz/file/beQAnBhT#hqUFJrznCZ_nHCq0YmjXC6-TUohzQtxlShuiEIbTcoI",
+},
+
+"Leia - Yuyoyuppe": {
+"category": "vocaloid",
+"linkMusescore": "https://musescore.com/user/18011801/scores/5622410",
+"linkMega": "https://mega.nz/file/uGgAlLYY#kO3XcrDt8RKk17sPIXkPnNYM477s-mV5s2xXwuw-LFo",
+},
+
+"Kaze no Toori Michi - Tonari no Totoro (restricted to Japan)": {
+"category": "anime",
+"linkMusescore": "https://musescore.com/user/18011801/scores/5723359",
+"linkMega": "https://mega.nz/file/7agFkCQC#Js424bZSIHfMj8JiNGJPzy_ouDHCT9P6Sy49f6GEne4",
+},
+
+"Yume to Hazakura - Aoki Gekkou": {
+"category": "vocaloid",
+"linkMusescore": "https://musescore.com/user/18011801/scores/5882252",
+"linkMega": "https://mega.nz/file/zf5kHTBJ#1HR0qJVzZfzPX0dmuWNQ3fxFaW84fYiS6LLl6-Q3lv4",
+},
+
+"Twaddle of a flue Faker - Steampianist ft.Oliver": {
+"category": "vocaloid",
+"linkMusescore": "https://musescore.com/user/18011801/scores/5891584",
+"linkMega": "https://mega.nz/file/HKBQWCQQ#lokhVmjgtDO4u4HMIRww5oxCI4hTJX6yLegYHiMS9qg",
+},
+
+"world.execute(me) | vocal and piano part - Milli": {
+"category": "modern",
+"linkMusescore": "https://musescore.com/user/18011801/scores/6238779",
+"linkMega": "https://mega.nz/file/7GIhlAKR#JtqfeVwH-Jzy-GvueaoxKel_k-YVaYuXoXfZiuZY4Ko",
+},
+
+"Kanawanu Koi - Domestic na Kanojo": {
+"category": "anime",
+"linkMega": "https://mega.nz/file/aL5FRZIB#42psaCx1RbUWNUjKRBx0SsbSiRoytt2veuEGRjfKYoE",
+},
+
+"Song on the Beach; Photograph - Her": {
+"category": "modern",
+"linkMega": "https://mega.nz/file/KfZUjQ6D#ulSEB-EI6OQKbXCNFFYhmwZpl8-s6ekya1Xi5a0E3dI",
+},
+
+"Bara Otome no Unmei - Rozen Maiden": {
+"category": "anime",
+"linkMusescore": "https://musescore.com/user/18011801/scores/6457277",
+"linkMega": "https://mega.nz/file/yCY3VA5I#ngblixYIff9G9_on495rNOwkb99DdkQ9uh2i9eUXT7I",
+},
+
+"Angel Song - Martin Landh": {
+"category": "modern",
+"linkMusescore": "https://musescore.com/user/18011801/scores/6462840",
+"linkMega": "https://mega.nz/file/7agDmRDK#hRC--NgZwWHPj1RuidqfonOU-iSBuOgQMQz4dx9FdZg",
+},
+
+"Elm - Martin Landh": {
+"category": "modern",
+"linkMusescore": "https://musescore.com/user/18011801/scores/6462847",
+"linkMega": "https://mega.nz/file/uO4xjbJR#bxmQdSMtu3yqZtL08VoJz12mpGtLsnxuKuncywtCNwY",
+},
+
+"City Ruins - NieR Automata Piano Collection (restricted to Japan)": {
+"category": "game",
+"linkMusescore": "https://musescore.com/user/18011801/scores/6535524",
+"linkMega": "https://mega.nz/file/iL5EgDpT#jQH2oGDIVTV2WCLDDcgaHdgjRuYXPobIixWQUQR2vkE",
+},
+
+"Unknow 1": {
+"category": "modern",
+"linkMusescore": "https://musescore.com/user/18011801/scores/6542702/s/kk7ry2",
+"linkMega": "https://mega.nz/file/qX4g0KDa#yZ1nTk2MGfGIvtjq8MAjvbLIyomP4TxapmtMSeQkXGI",
+},
+
+"Perfect - Ed Sheeran": {
+"category": "modern",
+"linkMusescore": "https://musescore.com/user/18011801/scores/6713400",
+"linkMega": "https://mega.nz/file/mfYXiZZA#9GJfKI8iD71qhsGYOa5Bvzn3sVqcUCzApOBeukpBQ5I",
+},
+
+"Shinkyuu Bunbougu Onna Taiketsu - Monogatari OST": {
+"category": "anime",
+"linkMega": "https://mega.nz/file/nWw2FLDS#EzvFBnosI9hBNQexGqDJyQBVKLWHilLEpH6pO3NIho4",
+},
+
+"Song of the Ancients - NieR Gestalt and Replicant Piano Collection": {
+"category": "game",
+"linkMusescore": "https://musescore.com/user/18011801/scores/6792076",
+"linkMega": "https://mega.nz/file/WPRCSAoT#uwYtrX16n8PO3qEtflIXr7jNNTZOhLknK2Eo1h64n9A",
+"linkMegaComment": "https://mega.nz/file/vSI0wKiT#EGDh-hKSdfdsKGwZ4WUIQw8A5ppXg8wawZfvI6fr_5Q",
+},
+
+"Ori Theme and Variation - Ori": {
+"category": "game",
+"linkMusescore": "https://musescore.com/user/18011801/scores/6926669",
+"linkMega": "https://mega.nz/file/aapVUAzL#lQDb0pJIRp3ALkDSDZckTPfrV1_QuXFJJLM6_uRLWQU",
+},
+
+"Tatakai no Hate - Rozen Maiden": {
+"category": "anime",
+"linkMusescore": "https://musescore.com/user/18011801/scores/7050346",
+"linkMega": "https://mega.nz/file/DDBgiJzZ#Spn8cAyZvfsNm306wEYmKwXDug0cXTmfrxHjLGcoMg4",
+},
+
+"Tanjou ~Kedakaki Dolls~ - Rozen Maiden": {
+"category": "anime",
+"linkMusescore": "https://musescore.com/user/18011801/scores/7052429",
+"linkMega": "https://mega.nz/file/OCoETDYR#cJbwkmSmu4ZZXGvxxxdVDLOdczkbOcG8nCRR2mRlOzw",
+},
+
+"Maboroshi no Ginzuishou - Sailor Moon, ayapiano98 arrange": {
+"category": "anime",
+"linkMega": "https://mega.nz/file/3GJXgY5b#Cmu3BSzqJDEkhIT-6TmO4ns-QH0f8irhQNDgZjcUl20",
+},
+
+"Hana ni Bourei (Piano accompaniment) - A Whisker Away": {
+"category": "anime",
+"linkMusescore": "https://musescore.com/user/18011801/scores/7155807",
+"linkMega": "https://mega.nz/file/KTR0zLSA#DRpncr8dVo1b2klST64szRMi3ZXc9S00h73NrVmDNC4",
+},
+
+"Kaine - NieR Gestalt and Replicant Piano Collection": {
+"category": "game",
+"linkMusescore": "https://musescore.com/user/18011801/scores/7741841",
+"linkMega": "https://mega.nz/file/aGImFTiK#xhkcm7jTZG6I9aUP-37nhD8jDQ0C4zIM_2ZKEFt6zHI",
+},
+
+"Unjust Life - Angel Beats": {
+"category": "anime",
+"linkMusescore": "https://musescore.com/user/18011801/scores/7760576",
+"linkMega": "https://mega.nz/file/uKplDBYJ#7QO-7ecqDgEbgbdeON0143vNF0nJTjuep7HUuyr2zVQ",
+},
+
+"Grandma - NieR Gestalt and Replicant Piano Collection": {
+"category": "game",
+"linkMusescore": "https://musescore.com/user/18011801/scores/7883366",
+"linkMega": "https://mega.nz/file/bWhQAIpZ#WcAoe9iGmPLkk5rOfWaDfYnuV7J8V3JFvCqpWlEwUBg",
+},
+
+"Hirari, Hirari - Tamaazu-p": {
+"category": "vocaloid",
+"linkMusescore": "https://musescore.com/user/18011801/scores/7940807",
+"linkMega": "https://mega.nz/file/SK4DTTyS#peuK9h0fIJsLnewWAqk49tuv6VgBOO_LzirL0MWmyvg",
+},
+
+"Sakura Moyu Title - Sakura Moyu": {
+"category": "game",
+"linkMusescore": "https://musescore.com/user/18011801/scores/7998329/s/aghNSI",
+"linkMega": "https://mega.nz/file/XGhzUZyS#uTLH5bonr6Axc6eFnj68lLvreVF39VP0I7ggzMp8ZLQ",
+},
+
+"Hare Nocchi Title - Hare Nocchi Kitto Nanohana Biyori": {
+"category": "game",
+"linkMega": "https://mega.nz/file/TWoB2LQK#D-0aIisXSh9byN5MKDkIWd9n4dnXtrHRSmOBlas_5Sw",
+},
+
+"Deemo Goodbye - Deemo": {
+"category": "game",
+"linkMusescore": "https://musescore.com/user/18011801/scores/8120601/s/ZegfAX",
+"linkMega": "https://mega.nz/file/SfImUA5a#YpuWLxYpxW-JsWIMQ6zCFdQJvKqrGtXrr0eRFWLX9ew",
+"linkMegaComment": "https://mega.nz/file/eX4kQBJS#As05pE5jBjsYfKbFznWK2FgYZfdBEB8nZ5lHd6bDLeE",
+},
+
+"Fluquor - Deemo Piano Collection": {
+"category": "game",
+"linkMusescore": "https://musescore.com/user/18011801/scores/8158449",
+"linkMega": "https://mega.nz/file/eeID1I6b#RjN56nnZwrDXMymzPMqA4fL2adOoP10FLQlw1jGyWh0",
+},
+
+"A Beautiful Song - NieR Automata Piano Collection": {
+"category": "game",
+"linkMusescore": "https://musescore.com/user/18011801/scores/8455760",
+"linkMega": "https://mega.nz/file/GCBzDbyJ#9Q_D4lHvnl_JlHX3TuyE8aqkgfxI_t0Bq2NFOdusbwU",
+},
+
+"Ikiru (Piano Accompaniment) - Mizuno Atsu": {
+"category": "modern",
+"linkMusescore": "https://musescore.com/user/18011801/scores/9100499",
+"linkMega": "https://mega.nz/file/XOBH2bxb#gBgxsb_FJqeUI7w2cNmmFGiJhEF_8mcjqeqKlB84TBg",
+},
+
+"Wilt - VerseQuence": {
+"category": "game",
+"linkMusescore": "https://musescore.com/user/18011801/scores/9401068",
+"linkMega": "https://mega.nz/file/POxlRIwb#dgFVGHty3S5jJ0ktcwXJiWT8SSKMU8kgvtf2ugkBSCw",
+},
+
+"1/6 Out of Gravity 16th Mix - Vocaloid-P": {
+"category": "vocaloid",
+"linkMusescore": "https://musescore.com/user/18011801/scores/10121296",
+"linkMega": "https://mega.nz/file/7fYiGLAD#ar9Gg_upqWecc3LyWOHAcc0K4OYSgdFj45QSDT3eyzg",
+},
+
+"Fall - Umineko no Naku Koro Ni": {
+"category": "game",
+"linkMusescore": "https://musescore.com/user/18011801/scores/11104315/s/_Bk6T5",
+"linkMega": "https://mega.nz/file/GaJhUY7b#fiGVSv-M-ooFafZkMRpytBDDcaCsQkX_6N_d7acY8m0",
+},
+
+"What was Lost - Chaos;Child": {
+"category": "game",
+"linkMusescore": "https://musescore.com/user/18011801/scores/11104345/s/W7wPgt",
+"linkMega": "https://mega.nz/file/meRjSZLA#Vp9Qnolg1dspGX9Rn5bdttAe6ZhmWrqseOHBQXXHRC0",
+},
+
+"Yubikiri Genman - Deemo Piano Collection": {
+"category": "game",
+"linkMusescore": "https://musescore.com/user/18011801/scores/11259133",
+"linkMega": "https://mega.nz/file/iSZ0hKzA#pYVRA4V415x2oZ6-B_POi5NFiSPFzsjv1lE4By4dyVY",
+},
+
+"Dependent Weakling - NieR Automata Piano Collection": {
+"category": "game",
+"linkMusescore": "https://musescore.com/user/18011801/scores/12228154",
+"linkMega": "https://mega.nz/file/zGIhES4J#qNAdt7tjLX3CSgRtcNXopXTtnL4z2GagRDDtdGmz0FM",
+},
+
+"Tsukiyo no Ongakukai ~Anata to Kimi to~ - The Liar Princess and the Blind Prince": {
+"category": "game",
+"linkMusescore": "https://musescore.com/user/18011801/scores/12684739",
+"linkMega": "https://mega.nz/file/mawmhZrY#xTpBulqVy0wOgz1MSACTiNiHEU1Q4yV-4Bf7xGxEt5w",
+"linkMegaComment": "https://mega.nz/file/KLxlhSBB#Vvmg-QJJeRiAkR6amei6NcI8dPK16_3q2Hk_YDfQhEM",
+},
+
+"River Flow in You - Yiruma": {
+"category": "modern",
+"linkMega": "https://mega.nz/file/mD5ggLwY#JOnL9208LfPY2-3m_lm4zOz4ao0uKCK7gw3LvRc45Ow",
+"linkMegaComment": "https://mega.nz/file/nO5mAKhZ#Of-Wz-fTp8gsfiJEuIVr_ul4AabELmm3O_NzK8fFCJY",
+},
+
+"Merry Christmas, Mr. Lawrence - Ryuichi Sakamoto": {
+"category": "modern",
+"linkMusescore": "https://musescore.com/user/18011801/scores/13251283",
+"linkMega": "https://mega.nz/file/nOBgwL7Y#Z7fgMtbpTEOF0VTfHDW4S_BAZn7Ejc3791qwDiRfXqQ",
+"linkMegaComment": "https://mega.nz/file/mGowXZDD#KIRuqahoFtm6XQqgiJuTv7BFkCMlBaBYGfxe1aPVsHM",
+},
+
+};
