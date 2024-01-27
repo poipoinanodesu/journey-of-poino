78e46fa8cce7d0fd3f83dcdf8833f73cc9d10069
poipoinanodesu <154312691+poipoinanodesu@users.noreply.github.com>
2024-01-27 14:54:59 +0700

    Add sorting feature
    Add sort feature with assistance of ChatGPT The Great.

--- assets/js/main_backup.js
+++ assets/js/main_backup.js
@@ -191,14 +191,30 @@
   /**
    * Porfolio isotope and filter
    */
-  const mySelect = document.querySelector("#sortOptions");
-  mySelect.addEventListener('change', () => {
-    filterContentChange();
-	let portfolioFilters = select('#portfolio-flters li', true);
-	portfolioFilters.forEach(function(el) {
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
           el.classList.remove('filter-active');
         });
-	portfolioFilters[0].classList.add('filter-active');
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+
+      }, true);
+    }
+
   });
 
   /**
@@ -240,52 +256,3 @@
   new PureCounter();
 
 })()
-
-function filterContentChange() {
-  const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
-  }
-
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
-      } else {
-        selectEl.addEventListener(type, listener)
-      }
-    }
-  }
-	
-    let portfolioContainer = select('.portfolio-container');
-	
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-      });
-
-      let portfolioFilters = select('#portfolio-flters li', true);
-
-      on('click', '#portfolio-flters li', function(e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
-        });
-        this.classList.add('filter-active');
-
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-
-      }, true);
-    }	
-}
-window.addEventListener('load', () => {
-	filterContentChange();
-});
